ffe7090c0bce4a0dd7956a87f5eb51a7d353d7a9
Gina Therese Hvidsten <8248605+gthvidsten@users.noreply.github.com>
2025-03-16 22:24:19 +0100

    Moved source code from main Cambion repository into its own to better be able to maintain and publish it without having to publish everything.

---
+++ test/IntegrationTests/AzureServiceBusTransportTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using RandomTestValues;
+using Xunit;
+
+namespace Whitestone.Cambion.Transport.AzureServiceBus.IntegrationTests
+{
+    public class AzureServiceBusTransportTests(AzureServiceBusFixture fixture) : IClassFixture<AzureServiceBusFixture>
+    {
+        [Fact]
+        public async Task PublishAsync_NullValue_ThrowsArgumentNullException()
+        {
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => { await fixture.Transport.PublishAsync(null); });
+        }
+
+        [Fact]
+        public async Task PublishAsync_SameDataReceived_Success()
+        {
+            // Arrange
+
+            ManualResetEvent mre = new(false);
+            byte[] expectedBytes = RandomValue.Array<byte>();
+
+            byte[] actualBytes = null;
+            fixture.Transport.MessageReceived += (_, e) =>
+            {
+                actualBytes = e.MessageBytes;
+                mre.Set();
+            };
+
+            // Act
+
+            await fixture.Transport.PublishAsync(expectedBytes);
+
+            // Assert
+
+            bool eventFired = mre.WaitOne(TimeSpan.FromSeconds(5));
+
+            Assert.True(eventFired);
+            Assert.Equal(expectedBytes, actualBytes);
+        }
+    }
+}
