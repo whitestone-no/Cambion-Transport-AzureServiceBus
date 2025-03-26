d982eb932d29d31ea32fea8f312e18e5b2b720e8
Gina Therese Hvidsten <8248605+gthvidsten@users.noreply.github.com>
2025-03-26 14:49:22 +0100

    Added methods to read from configuration

--- src/AzureServiceBusTransportExtensions.cs
+++ src/AzureServiceBusTransportExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Whitestone.Cambion.Interfaces;
@@ -9,7 +10,29 @@ namespace Whitestone.Cambion.Transport.AzureSericeBus
     {
         public static ICambionSerializerBuilder UseAzureServiceBusTransport(this ICambionTransportBuilder builder, Action<AzureServiceBusConfig> configure)
         {
+            return UseAzureServiceBusTransport(builder, null, configure, null);
+        }
+
+        public static ICambionSerializerBuilder UseAzureServiceBusTransport(this ICambionTransportBuilder builder, IConfiguration configuration, string cambionConfigurationKey = "Cambion")
+        {
+            return UseAzureServiceBusTransport(builder, configuration, null, cambionConfigurationKey);
+        }
+
+        public static ICambionSerializerBuilder UseAzureServiceBusTransport(this ICambionTransportBuilder builder, IConfiguration configuration, Action<AzureServiceBusConfig> configure, string cambionConfigurationKey = "Cambion")
+        {
             builder.Services.Replace(new ServiceDescriptor(typeof(ITransport), typeof(AzureServiceBusTransport), ServiceLifetime.Singleton));
+
+            if (configuration != null)
+            {
+                string assemblyName = typeof(AzureServiceBusTransport).Assembly.GetName().Name;
+
+                IConfigurationSection config = configuration.GetSection(cambionConfigurationKey).GetSection("Transport").GetSection(assemblyName);
+
+                if (config.Exists())
+                {
+                    builder.Services.Configure<AzureServiceBusConfig>(config);
+                }
+            }
 
             builder.Services.AddOptions<AzureServiceBusConfig>()
                 .Configure(conf =>
